c8829dd26cc05bda83998c27a7e8f846f73e67d3
Phil McLean <phil@blockstream.com>
2024-10-27 18:29:41 -0700

    Use ip from hotel

--- esplora/docker-entrypoint.sh
+++ esplora/docker-entrypoint.sh
@@ -5,6 +5,6 @@
 # wait-for-it.sh 10.10.37.66:18445 -t 0
 # elementsd
 # PUT YOUR IP OF HOST HERE
-wait-for-it.sh 10.10.37.66:18886 -t 0
+wait-for-it.sh 10.152.176.171:18886 -t 0
 
 exec /srv/explorer/run.sh $@
