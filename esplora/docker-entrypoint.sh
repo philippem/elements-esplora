9010d389ec3d707390251817a7e468c6167a79a0
Phil McLean <phil@blockstream.com>
2024-10-27 18:26:54 -0700

    connect to elements on host

--- esplora/docker-entrypoint.sh
+++ esplora/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # bitcoind
-wait-for-it.sh 172.16.231.10:18445 -t 0
+# PUT YOUR IP HERE 
+# wait-for-it.sh 10.10.37.66:18445 -t 0
 # elementsd
-wait-for-it.sh 172.16.231.11:18443 -t 0
+# PUT YOUR IP OF HOST HERE
+wait-for-it.sh 10.10.37.66:18886 -t 0
 
 exec /srv/explorer/run.sh $@
